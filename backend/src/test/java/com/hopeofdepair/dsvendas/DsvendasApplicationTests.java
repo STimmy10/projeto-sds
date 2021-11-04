b2c97648e5d57f67aea7004e8400c2c0db51b2aa
Lucas Lucena <lucasdaibes@yahoo.com.br>
2021-11-03 21:35:15 -0300

    Project created

---
+++ backend/src/test/java/com/hopeofdepair/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopeofdepair.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
