ed6e63b77a9d0f626de673bf05dacdaed74c2f9b
STimmy10 <lucasdaibes@yahoo.com.br>
2021-11-04 00:18:23 -0300

    Basic Static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import NavBar from 'components/NavBar';
+import Footer from 'components/Footer';
+import DataTable from 'components/DataTable';
 import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h2 className="text-primary">Olá mundo</h2>
-    </div>
+    <>
+      <div className = "container">
+        <NavBar />
+        <h1 className="text-primary">Olá mundo</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
