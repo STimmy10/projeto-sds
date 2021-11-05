9b14a51b5e77484f90f77cedd6d43dfdc90ef5cd
STimmy10 <lucasdaibes@yahoo.com.br>
2021-11-04 22:56:13 -0300

    Layers

---
+++ backend/src/main/java/com/hopeofdepair/dsvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.hopeofdepair.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hopeofdepair.dsvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
